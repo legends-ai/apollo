1e5d1fef0f18a2b9aa24c6c4afec4d9513116d18
Ian Macalinao <me@ian.pw>
2016-09-06 00:48:16 -0500

    Implement aggregation from filters

--- aggregation/aggregator.go
+++ aggregation/aggregator.go
@@ -2,6 +2,7 @@ package aggregation
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/gocql/gocql"
 	"github.com/golang/protobuf/proto"
@@ -24,9 +25,39 @@ type Aggregator struct {
 
 // GenerateAggregate generates an aggregate for a champion.
 func (a *Aggregator) AggregateFromFilters(filters []*apb.MatchFilters) (*apb.MatchAggregate, error) {
+	// Channel containing sums
+	sumsChan := make(chan *apb.MatchSum)
+
+	// Concurrently fetch all sums
+	var wg sync.WaitGroup
+	wg.Add(len(filters))
+
+	// Iterate over all filters
+	for _, filter := range filters {
+
+		// Asynchronous get
+		go func(filter *apb.MatchFilters) {
+			sumsChan <- a.fetchSum(filter)
+			wg.Done()
+		}(filter)
+	}
+
+	// Create aggregate sum
+	sum := &apb.MatchSum{}
+	go func() {
+		for sumRow := range sumsChan {
+			sum = addSums(sum, sumRow)
+		}
+	}()
+
+	// Terminate when all sums are fetched
+	wg.Wait()
+	close(sumsChan)
+
+	return buildAggregate(sum)
 }
 
-func (a *Aggregator) fetchRow(f *apb.MatchFilters) (*apb.MatchSum, error) {
+func (a *Aggregator) fetchSum(f *apb.MatchFilters) (*apb.MatchSum, error) {
 	var rawSum []byte
 	if err := a.CQL.Query(
 		stmtGetSum, f.ChampionId, f.EnemyId, f.Patch,
@@ -46,4 +77,8 @@ func (a *Aggregator) fetchRow(f *apb.MatchFilters) (*apb.MatchSum, error) {
 func addSums(a, b *apb.MatchSum) *apb.MatchSum {
 	// TODO(igm): add sums
 	return a
+}
+
+func buildAggregate(sum *apb.MatchSum) (*apb.MatchAggregate, error) {
+	return nil, nil
 }
